601894b437c8b0e885a72a86add811d18bd30636
leonel ka <waboleonel@gmail.com>
2025-02-15 12:02:15 +0100

    Change table name

--- src/main/java/com/wbt/todo_app/models/Todo.java
+++ src/main/java/com/wbt/todo_app/models/Todo.java
@@ -3,6 +3,7 @@ package com.wbt.todo_app.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "todos")
 public class Todo {
     @Id
     @GeneratedValue
