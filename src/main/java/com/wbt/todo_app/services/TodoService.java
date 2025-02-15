c0202f8d6f66793f48fb55a7fe75f9647dd8d6f4
leonel ka <waboleonel@gmail.com>
2025-02-15 12:04:40 +0100

    Define service methods

--- src/main/java/com/wbt/todo_app/services/TodoService.java
+++ src/main/java/com/wbt/todo_app/services/TodoService.java
@@ -1,4 +1,20 @@
 package com.wbt.todo_app.services;
 
+import com.wbt.todo_app.models.Todo;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface TodoService {
+    Todo create(final Todo todo);
+
+    List<Todo> getAllTodos();
+
+    Todo fetchById(final UUID todoId);
+
+    Todo fetchByTitle(final String todoTitle);
+
+    Integer deleteById(final UUID todoId);
+
+    Todo update(final UUID todoId, final Todo todo);
 }
