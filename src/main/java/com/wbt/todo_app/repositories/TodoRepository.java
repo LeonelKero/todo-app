13b300f272dabfc8b6c49abd0c7e5f952f7972e1
leonel ka <waboleonel@gmail.com>
2025-02-15 12:05:06 +0100

    Add custom method to the repository

--- src/main/java/com/wbt/todo_app/repositories/TodoRepository.java
+++ src/main/java/com/wbt/todo_app/repositories/TodoRepository.java
@@ -4,8 +4,10 @@ import com.wbt.todo_app.models.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, UUID> {
+    Optional<Todo> findByTitle(String title);
 }
