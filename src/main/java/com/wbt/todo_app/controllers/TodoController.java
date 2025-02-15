a5cb52353029a086292dcbd61527e0cb95b557fb
leonel ka <waboleonel@gmail.com>
2025-02-15 12:04:15 +0100

    Implement controller

--- src/main/java/com/wbt/todo_app/controllers/TodoController.java
+++ src/main/java/com/wbt/todo_app/controllers/TodoController.java
@@ -2,11 +2,9 @@ package com.wbt.todo_app.controllers;
 
 import com.wbt.todo_app.models.Todo;
 import com.wbt.todo_app.services.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -20,16 +18,32 @@ public class TodoController {
     }
 
     @GetMapping
-    public String getTodos() {
-        return "Getting all todos!";
+    public List<Todo> getTodos() {
+        return this.service.getAllTodos();
     }
 
     @PostMapping
-    public Todo add() {
-        return new Todo(
-                UUID.randomUUID(),
-                "Master Spring Boot",
-                "Get hands on spring boot API development",
-                false);
+    public Todo add(final @RequestBody Todo todoRequest) {
+        return this.service.create(todoRequest);
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public Todo getById(final @PathVariable UUID id) {
+        return this.service.fetchById(id);
+    }
+
+    @GetMapping(path = {"/get/{title}"})
+    public Todo getById(final @PathVariable String title) {
+        return this.service.fetchByTitle(title);
+    }
+
+    @DeleteMapping(path = {"/{id}"})
+    public void remove(final @PathVariable UUID id) {
+        this.service.deleteById(id);
+    }
+
+    @PutMapping(path = {"/{id}"})
+    public Todo update(final @PathVariable UUID id, final @RequestBody Todo requestTodo) {
+        return this.service.update(id, requestTodo);
     }
 }
