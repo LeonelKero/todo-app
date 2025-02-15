0df9536db5b16500eaa821f01a28ab8165f8c306
leonel ka <waboleonel@gmail.com>
2025-02-15 12:05:26 +0100

    Implement service methods

--- src/main/java/com/wbt/todo_app/services/impl/TodoServiceImpl.java
+++ src/main/java/com/wbt/todo_app/services/impl/TodoServiceImpl.java
@@ -1,8 +1,12 @@
 package com.wbt.todo_app.services.impl;
 
+import com.wbt.todo_app.models.Todo;
 import com.wbt.todo_app.repositories.TodoRepository;
 import com.wbt.todo_app.services.TodoService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TodoServiceImpl implements TodoService {
@@ -11,5 +15,42 @@ public class TodoServiceImpl implements TodoService {
 
     public TodoServiceImpl(TodoRepository repository) {
         this.repository = repository;
+    }
+
+    @Override
+    public Todo create(final Todo todo) {
+        return this.repository.save(todo);
+    }
+
+    @Override
+    public List<Todo> getAllTodos() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public Todo fetchById(final UUID todoId) {
+        return this.repository.findById(todoId).orElse(null);
+    }
+
+    @Override
+    public Todo fetchByTitle(final String todoTitle) {
+        return this.repository.findByTitle(todoTitle).orElse(null);
+    }
+
+    @Override
+    public Integer deleteById(UUID todoId) {
+        return this.repository.findById(todoId)
+                .map(todo -> {
+                    this.repository.delete(todo);
+                    return 1;
+                })
+                .orElse(0);
+    }
+
+    @Override
+    public Todo update(UUID todoId, Todo request) {
+        return this.repository.findById(todoId)
+                .map(todo -> this.repository.save(request))
+                .orElse(null);
     }
 }
