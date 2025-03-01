f2c9e0c19aa2cb1c5f0938499a60b53ffccd1884
leonel ka <waboleonel@gmail.com>
2025-03-01 11:43:22 +0100

    Improve repository tests

--- src/test/java/com/wbt/todo_app/repositories/TodoRepositoryTest.java
+++ src/test/java/com/wbt/todo_app/repositories/TodoRepositoryTest.java
@@ -16,17 +16,52 @@ class TodoRepositoryTest {
 
     @BeforeEach
     void setUp() {
-        this.underTest.save(new Todo("Master JUnit5 Test", "Get hands on application testing"));
+        this.underTest.save(new Todo(
+                "Master JUnit5 Test",
+                "Get hands on application testing"));
     }
 
     @Test
-    void findByTitle() {
+    void whenDataIsIntoDatabase_FindingByTitle_ReturnValueInOptional() {
         // Given
         final var title = "Master JUnit5 Test";
+
         // When
         final var result = this.underTest.findByTitle(title);
+
         // Then
         assertThat(result.isPresent()).isTrue();
+        assertThat(result.get().getTitle()).isEqualTo(title);
+    }
+
+    @Test
+    void whenDataIsNotIntoDatabase_FindingByTitle_ReturnEmptyOptional() {
+        // Given
+        final var fakeTitle = "Not existing title";
+
+        // When
+        final var response = this.underTest.findByTitle(fakeTitle);
+
+        // Then
+        assertThat(response.isEmpty()).isTrue();
+    }
+
+    @Test
+    void withValidTodoId_FindingById_ReturnTodoObject() {
+        // Given
+        final var newTodo = new Todo(
+                "Lean Spring Boot 3",
+                "Hands on enterprise application development.");
+        final var id = this.underTest.save(newTodo).getId();
+
+        // When
+        final var optionalTodo = this.underTest.findById(id);
+
+        // Then
+        assertThat(optionalTodo.isPresent()).isTrue();
+        assertThat(optionalTodo.get().getTitle()).isEqualTo(newTodo.getTitle());
+        assertThat(optionalTodo.get().getDescription()).isEqualTo(newTodo.getDescription());
+        assertThat(optionalTodo.get().getIsDone()).isFalse();
     }
 
     @Test
