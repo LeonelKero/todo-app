75132b306d02abdd2b1cc0a984c85ffc38400ac8
leonel ka <waboleonel@gmail.com>
2025-03-06 22:06:04 +0100

    Add validation annotation to todo request

--- src/main/java/com/wbt/todo_app/dto/TodoRequest.java
+++ src/main/java/com/wbt/todo_app/dto/TodoRequest.java
@@ -1,4 +1,17 @@
 package com.wbt.todo_app.dto;
 
-public record TodoRequest(String title, String description, Boolean done) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TodoRequest(
+        @NotBlank(message = "Todo title is required")
+        @Size(min = 3, message = "Todo title should be at least 03 characters")
+        String title,
+
+        @NotBlank(message = "Todo description is required")
+        String description,
+
+        @NotNull(message = "Todo state is required. done: true | false")
+        Boolean done) {
 }
