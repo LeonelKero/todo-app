e4abec45413bfe52695a1375f25adf1c2235fe28
leonel ka <waboleonel@gmail.com>
2025-03-20 10:11:24 +0100

    Temporally disable CORS

--- src/main/java/com/wbt/todo_app/controllers/TodoController.java
+++ src/main/java/com/wbt/todo_app/controllers/TodoController.java
@@ -13,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping(path = {"/api/v1/todos"})
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class TodoController {
 
     private final TodoService service;
